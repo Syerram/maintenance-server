eb82c3e55564ffe3506931e83f75d948121637dd
Saikiran Yerram <sai.r.yerram@gmail.com>
2013-05-04 19:59:17 -0400

    adding release env to check for debug

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
@@ -12,4 +13,8 @@ def maintenance():
   return render_template('maintenance.html'), 503 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8040, debug=True)
+  release = os.environ.get('RELEASE', 'DEV')
+  debug = release == 'DEV'
+  print 'running in debug mode? %s' % debug
+  app.run(host='0.0.0.0', port=8040, debug=debug)
+
